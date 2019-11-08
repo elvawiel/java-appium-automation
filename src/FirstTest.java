003c7f8782e49431ae6827dbd9d4982a6ab50b76
Maryna Afanasyeva <elvawiel@gmail.com>
2019-11-08 16:48:07 +0300

    Moved screen orientation to setUp

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -34,16 +34,12 @@ public class FirstTest {
         capabilities.setCapability("app", "C:/Users/elvaw/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.rotate(ScreenOrientation.PORTRAIT);
     }
 
     @After
     public void tearDown()
     {
-        ScreenOrientation orientation = driver.getOrientation();
-        if (orientation.toString() != "PORTRAIT")
-        {
-            driver.rotate(ScreenOrientation.PORTRAIT);
-        }
         driver.quit();
     }
 
